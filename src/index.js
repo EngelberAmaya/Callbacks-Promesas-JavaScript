cefbb82e4bf441f8fcc7dbff47193ab894333d0e
EngelberAmaya <engelberamaya@gmail.com>
2023-05-01 16:08:53 -0400

    async

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
+import { buscarHeroe, buscarHeroeAsync } from './js/promesas';
 import './styles.css';
-import { promesaLenta, promesaMedia, promesaRapida } from "./js/promesas";
+//import { promesaLenta, promesaMedia, promesaRapida } from "./js/promesas";
+
+
 
 // promesaLenta.then(console.log);
 // promesaMedia.then(console.log);
 // promesaRapida.then(console.log);
 
-Promise.race([promesaLenta, promesaMedia, promesaRapida])
-    .then(console.log)
-    .catch(console.warn)
+// Promise.race([promesaLenta, promesaMedia, promesaRapida])
+//     .then(console.log)
+//     .catch(console.warn)
+
+buscarHeroe('capi').then(heroe => console.log(heroe))
+    .catch(console.warn);
+
+buscarHeroeAsync('iron2').then(heroe => console.log(heroe))
+    .catch(console.warn);
