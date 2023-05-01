8fd0ea903a13401b13dc7db3acc26508d56c5df2
EngelberAmaya <engelberamaya@gmail.com>
2023-05-01 15:32:40 -0400

    promise.race

--- src/js/promesas.js
+++ src/js/promesas.js
@@ -25,5 +25,22 @@ export const buscarHeroe = (id) => {
         }
     })
 
+}
 
+const promesaLenta = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('Promesa lenta'), 2000);
+});
+
+const promesaMedia = new Promise((resolve, reject) => {
+    setTimeout(() => resolve('Promesa Media'), 1500);
+});
+
+const promesaRapida = new Promise((resolve, reject) => {
+    setTimeout(() => reject('Promesa Rapida'), 1000);
+});
+
+export {
+    promesaLenta,
+    promesaMedia,
+    promesaRapida
 }
